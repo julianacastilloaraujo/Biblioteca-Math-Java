878991c46bd8d8ad6ec2eb18beb009404466761f
Juliana Castillo <96964513+jlianacastillo@users.noreply.github.com>
2022-01-05 16:10:37 -0500

    Añadi archivo BibliotecaMath.java

---
+++ BibliotecaMath.java
@@ -0,0 +1,18 @@
+package BibliotecaMath;
+import java.util.Scanner;
+import java.math.*;
+public class BibliotecaMath {
+
+    public static void main(String[] args) {
+        int a=40;
+        int b=11;
+        System.out.println("el numero max de dos numeros es "+(Math.max(a,b)));
+        int Resultado =(Math.max(a,b));
+        System.out.println("otra forma"+Resultado); 
+        double Potencia=(Math.pow(3,2));
+        System.out.println("el valor de la potencia es "+Potencia);
+        double seno=(Math.sin(90));
+        System.out.println("el valor del seno de 90 es"+seno);
+    }
+    
+}
